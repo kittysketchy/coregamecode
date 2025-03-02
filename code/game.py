6e1fcc6d358b1a26bafd508e4b5a5b83a6ab44a2
kitty <rudrajit174@gmail.com>
2025-03-02 11:13:01 +0530

    refactor code to handle input efficiently

--- code/game.py
+++ code/game.py
@@ -1,7 +1,8 @@
-import sys, pygame
+import pygame
 
 from player import Player
 from blocks import Block
+from input import Input
 from camera import Camera
 
 class Game:
@@ -24,12 +25,9 @@ class Game:
 
         self.player = Player()
 
-        self.renderables = [
-            Block(140, 10, 1000, 50, 'black'),
-            Block(140, 760, 1000, 50, 'black'),
-            Block(140, 60, 50, 700, 'black'),
-            Block(1090, 60, 50, 700, 'black')
-        ]
+        self.renderables = [Block(140, 10, 1000, 50, 'black'), Block(140, 760, 1000, 50, 'black'), Block(140, 60, 50, 700, 'black'), Block(1090, 60, 50, 700, 'black')]
+
+        self.input = Input()
 
         self.camera = Camera()
     
@@ -49,46 +47,10 @@ class Game:
             self.gather_renderables()
             
             # Watches for events
-            self.check_events()
+            self.input.check_events(self.player)
 
             # Redraws the screen during each pass through the loop
             self.update_screen()
-
-    
-    def check_events(self):
-        # Responds to keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                self.check_keydown_events(event)
-            elif event.type == pygame.KEYUP:
-                self.check_keyup_events(event)
-
-    
-    def check_keydown_events(self, event):
-        # Responds to keypresses
-        if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-            self.player.moving_left = True
-        elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-            self.player.moving_right = True
-        elif event.key == pygame.K_w or event.key == pygame.K_UP:
-            self.player.moving_up = True
-        elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-            self.player.moving_down = True
-        
-
-    def check_keyup_events(self, event):
-        # Responds to key releases
-        if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-            self.player.moving_left = False
-        elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-            self.player.moving_right = False
-        elif event.key == pygame.K_w or event.key == pygame.K_UP:
-            self.player.moving_up = False
-        elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-            self.player.moving_down = False
         
 
     def update_screen(self):
---
+++ code/input.py
@@ -0,0 +1,43 @@
+import sys, pygame
+
+class Input:
+    # Main class to manage player input
+
+    def __init__(self):
+        pass
+
+
+    def check_events(self, player):
+        # Responds to keyboard and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self.check_keydown_events(event, player)
+            elif event.type == pygame.KEYUP:
+                self.check_keyup_events(event, player)
+
+    
+    def check_keydown_events(self, event, player):
+        # Responds to keypresses
+        if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+            player.moving_left = True
+        elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+            player.moving_right = True
+        elif event.key == pygame.K_w or event.key == pygame.K_UP:
+            player.moving_up = True
+        elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
+            player.moving_down = True
+        
+
+    def check_keyup_events(self, event, player):
+        # Responds to key releases
+        if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+            player.moving_left = False
+        elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+            player.moving_right = False
+        elif event.key == pygame.K_w or event.key == pygame.K_UP:
+            player.moving_up = False
+        elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
+            player.moving_down = False
